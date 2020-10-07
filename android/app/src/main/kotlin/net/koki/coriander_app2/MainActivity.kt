5d452829ee1e5c1aec16bbf6c89c5efbe7059114
KOKI425 <Yashinoki_2000>
2020-10-07 23:37:05 +0900

    second commit

---
+++ android/app/src/main/kotlin/net/koki/coriander_app2/MainActivity.kt
@@ -0,0 +1,6 @@
+package net.koki.coriander_app2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_model.dart
@@ -0,0 +1,11 @@
+
+import 'package:flutter/cupertino.dart';
+
+class MainModel extends ChangeNotifier{
+  String kboyText = 'KBOYさんじゃないよ';
+
+  void changeKboyText(){
+    kboyText = 'Kboyさんこんにちは!!!';
+    notifyListeners();
+  }
+}
